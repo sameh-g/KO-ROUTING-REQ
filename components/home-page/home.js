8806c353bfb8134c255afcef4699db7a9eebf98a
Sameh George <sameh.george@itworx.com>
2016-10-27 16:53:07 +0200

    add loading

--- components/home-page/home.js
+++ components/home-page/home.js
@@ -1,12 +1,14 @@
 define(["knockout", "text!./home.html"], function(ko, homeTemplate) {
 
   function HomeViewModel(route) {
-    this.message =ko.observable('10%');
+     this.isLoading= ko.observable(true);
+     this.message =ko.observable('10%');
   }
 
   HomeViewModel.prototype.doSomething = function() {
-    this.message('You invoked doSomething() on the viewmodel.');
-  };
+
+ this.isLoading(false)
+    this.message('You invoked doSomething() on the viewmodel.'); };
 
   return { viewModel: HomeViewModel, template: homeTemplate };
 
