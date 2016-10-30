e15dcde9443a2a182b37b1675f37a3524e8fcce3
Sameh George <sameh.george@itworx.com>
2016-10-30 17:14:39 +0200

    commit authentications
    404 implementation page
    
    Active directory imp

--- components/home-page/home.js
+++ components/home-page/home.js
@@ -1,5 +1,21 @@
 define(["knockout", "text!./home.html"], function(ko, homeTemplate) {
 
+//Authorization popup window code
+ function authenticate(options)
+{
+    options.windowName = options.windowName ||  'ConnectWithOAuth'; // should not include space for IE
+    options.windowOptions = options.windowOptions || 'location=0,status=0,width=800,height=400';
+    options.callback = options.callback || function(){ window.location.reload(); };
+    var that = this;
+    log(options.path);
+    that._oauthWindow = window.open(options.path, options.windowName, options.windowOptions);
+    that._oauthInterval = window.setInterval(function(){
+        if (that._oauthWindow.closed) {
+            window.clearInterval(that._oauthInterval);
+            options.callback();
+        }
+    }, 1000);
+};
   function HomeViewModel(route) {
     
      this.message =ko.observable('10%');
--- server.js
+++ server.js
@@ -2,6 +2,62 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var sql = require('mssql');
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get our config file
+// var User   = require('./app/models/user'); // get our mongoose model
+var passport = require('passport');
+var WindowsStrategy = require('passport-windowsauth');
+
+app.set('superSecret', config.secret); // secret variable
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+var user ={Name:'sameh'}
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app.post('/login',
+//   passport.authenticate('WindowsAuthentication', {
+//     successRedirect: '/loginSuccess',
+//     failureRedirect: '/loginFailure'
+//   })
+// );
+
+// app.get('/loginFailure', function(req, res, next) {
+//   res.send('Failed to authenticate');
+// });
+
+// app.get('/loginSuccess', function(req, res, next) {
+//   res.send('Successfully authenticated');
+// });
+
+
+// passport.serializeUser(function(user, done) {
+//     done(null, user);
+// });
+
+// passport.deserializeUser(function(user, done) {
+//     done(null, user);
+// });
+
+// passport.use(new WindowsStrategy({
+//     integrated: true 
+// }, function(profile,done) {
+//     var user = {
+//         id: profile.id,
+//     };
+//     done(null, user);
+// }));
+
+// app.all("*", passport.authenticate("WindowsAuthentication"), function (request,response,next){
+//     next();
+// });
+
 
     var dbConfig = {
         server : "SGEORGE-8-8777",
@@ -24,7 +80,13 @@ app.use("/Navigation-page", express.static(__dirname + '/Navigation-page'));
 app.use("/Test-cr", express.static(__dirname + '/Test-cr'));
 
 
+var userName = process.env['USERPROFILE'].split(path.sep)[2];
+var loginId = path.join("domainName",userName);
+console.log('loginid**********',loginId);
 
+app.get('/authenticate', function(req, res) {
+          res.send("authenticate with "+loginId);
+}); 
 app.get('/index', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 }); 
@@ -32,6 +94,27 @@ app.get('/index', function(req, res) {
 
 
 app.get('/getValue', function(req, res) {
+
+        // if user is found and password is right so that we authenticating our service now.. 
+        // create a token
+        var token = jwt.sign(user, app.get('superSecret'), {
+           expiresIn : 60*60*24// Expiration Time.. 
+        });
+
+        console.log('**token Value**',token)
+      // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        console.log(decoded.Name)
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        // next();
+      }
+    });
+
+
 GetReportedCRMCases(res)
 });
 
@@ -58,6 +141,12 @@ function GetReportedCRMCases(res) {
     })
 }
 
+
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+      res.sendFile(path.join(__dirname + '/404.html'));
+});
 
 
 app.listen(3000, function () {
